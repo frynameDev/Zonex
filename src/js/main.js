25f741987e74eb8342bb40fad6ffcb2ea39863ad
frynameDev <psimen1986@gmail.com>
2021-05-12 01:08:24 +0300

    finisfed catalog-filter

--- src/js/main.js
+++ src/js/main.js
@@ -15,61 +15,84 @@ const catalogSlider = new Swiper('.hero-catalog__slider', {
   slidesPerView: 1,
 
 });
+//
+//catalog-filter-toggle
+const catalogFiltersTop = document.querySelectorAll('.catalog-filter__top');
+
+catalogFiltersTop.forEach(el => {
+  el.addEventListener('click', (e) => {
+    e.currentTarget.closest('.catalog-filter').classList.toggle('catalog-filter--open');
+  });
+});
+//
+// hide filters
+const hideFilters = document.querySelector('.hide-filters');
+
+hideFilters.addEventListener('click', (e) => {
+  catalogFiltersTop.forEach(el => {
+    el.closest('.catalog-filter').classList.remove('catalog-filter--open');
+  });
+});
+
+//
+
 // close
 const marketing = document.querySelector('.marketing');
 
-let counter = 0;
-let delay = 4000;
+if (marketing) {
+  let counter = 0;
+  let delay = 4000;
 
-const data = [
-  {
-    title: 'Title of product 1',
-    where: 'Moscow, Russia'
-  },
-  {
-    title: 'Title of product 2',
-    where: 'Kiev, Ukraine'
-  },
-  {
-    title: 'Title of product 3',
-    where: 'Rome, Italy'
-  },
-];
+  const data = [
+    {
+      title: 'Title of product 1',
+      where: 'Moscow, Russia'
+    },
+    {
+      title: 'Title of product 2',
+      where: 'Kiev, Ukraine'
+    },
+    {
+      title: 'Title of product 3',
+      where: 'Rome, Italy'
+    },
+  ];
 
-const closeMarketing = () => {
-  marketing.classList.remove('marketing--visible')
-}
-
-const changeMarketingData = () => {
-  marketing.classList.remove('marketing--visible')
-
-  setTimeout(() => {
-    marketing.classList.add('marketing--visible')
-  }, delay - 2000);
-
-
-  const stringTitle = `${data[counter].title}`;
-  const stringWhere = `15 minutes ago ${data[counter].where}`;
-
-  marketing.querySelector('.marketing__title').textContent = stringTitle;
-  marketing.querySelector('.marketing__when-from').textContent = stringWhere;
-
-  counter++;
-
-  if (counter === data.length) {
-    counter = 0;
+  const closeMarketing = () => {
+    marketing.classList.remove('marketing--visible')
   }
-}
 
-changeMarketingData();
+  const changeMarketingData = () => {
+    marketing.classList.remove('marketing--visible')
 
-setInterval(changeMarketingData, delay);
+    setTimeout(() => {
+      marketing.classList.add('marketing--visible')
+    }, delay - 2000);
 
-marketing.addEventListener('click', (e) => {
-  if (e.target.classList.contains('marketing__close')) {
-    closeMarketing();
+
+    const stringTitle = `${data[counter].title}`;
+    const stringWhere = `15 minutes ago ${data[counter].where}`;
+
+    marketing.querySelector('.marketing__title').textContent = stringTitle;
+    marketing.querySelector('.marketing__when-from').textContent = stringWhere;
+
+    counter++;
+
+    if (counter === data.length) {
+      counter = 0;
+    }
   }
-});
+
+  changeMarketingData();
+
+  setInterval(changeMarketingData, delay);
+
+  marketing.addEventListener('click', (e) => {
+    if (e.target.classList.contains('marketing__close')) {
+      closeMarketing();
+    }
+  });
+}
 
 
 //
